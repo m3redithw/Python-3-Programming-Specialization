11ab538e5f4c9a25d398336fef9c60bcaed46e6e
Meredith Wang <wang.meredith09@gmail.com>
2022-05-29 22:57:43 -0500

    added course 1 assessment 9

---
+++ course_1_assessment_9.py
@@ -0,0 +1,17 @@
+# q1. Which method would you use to figure out the position of an item in a list?
+# .index() 
+
+# q2. Which method is best to use when adding an item to the end of a list?
+# .append()
+
+# q3. Write code to add ‘horseback riding’ to the third position (i.e., right before volleyball) in the list sports.
+sports = ['cricket', 'football', 'volleyball', 'baseball', 'softball', 'track and field', 'curling', 'ping pong', 'hockey']
+sports.insert(2, 'horseback riding')
+
+# q4. Write code to take ‘London’ out of the list trav_dest.
+trav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathmandu', 'Osaka', 'London', 'Melbourne']
+trav_dest.remove('London')
+
+# q5. Write code to add ‘Guadalajara’ to the end of the list trav_dest using a list method.
+trav_dest = ['Beirut', 'Milan', 'Pittsburgh', 'Buenos Aires', 'Nairobi', 'Kathmandu', 'Osaka', 'Melbourne']
+trav_dest.append('Guadalajara' )
