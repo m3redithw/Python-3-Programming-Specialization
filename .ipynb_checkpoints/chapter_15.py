71d274717f654354fd58ccb7850764032b983e30
Meredith Wang <wang.meredith09@gmail.com>
2022-06-03 22:28:04 -0500

    added chapter15 notes

---
+++ .ipynb_checkpoints/chapter_15.py
@@ -0,0 +1,13 @@
+# What will the following code print?
+def f(x = 0, y = 1):
+    return x * y
+
+print(f())
+# 0
+
+# What will the following code print?
+def f(x = 0, y = 1):
+    return x * y
+
+print(f(1))
+# 1
